24ab7416d4ef817d62e5f5779cc0d70293ce4b52
Biswajeet2004195 <biswajeetbehera291@gmail.com>
2026-07-15 22:02:14 +0530

    remove temporary test and migration scripts

--- make_test.py
+++
@@ -1,9 +0,0 @@
-import re
-with open("regform.py", "r", encoding="utf-8") as f:
-    content = f.read()
-
-# Remove root.mainloop()
-content = content.replace("root.mainloop()", "print('Init complete!')")
-
-with open("test_regform.py", "w", encoding="utf-8") as f:
-    f.write(content)
--- test6.py
+++
@@ -1,2 +0,0 @@
-pwd_str = "pbkdf2:$5c4efaa65f317b9e8aef0a0de911c05c:$edb388ac7d8019c4230866901e4f66c54ca3572bc5f298b34f2657ce7a4b2607"
-print(pwd_str.split(':$'))
